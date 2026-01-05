16b0841d5baaa4b49a4282444937654f5aabf5d0
yuusan33 <214938543+yuusan33@users.noreply.github.com>
2026-01-05 22:47:26 +0900

    編集ボタン作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,27 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  # editアクション  タスク編集画面表示
+  def edit
+    @task =Task.find(params[:id])
+  end
+
+  # updateアクション  タスク更新
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: 'タスクを更新しました'
+    else
+      render :edit
+    end
+  rescue ActiveRecord::RecordNotFound
+    redirect_to tasks_path, alert: 'タスクが見つかりません'
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  resources :tasks, only: [:index]
+  root "tasks#index"
+  resources :tasks, only: [:index, :edit, :update]
 end
