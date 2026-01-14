27c28b358c87819bff7ca6e0553723bf0ccf54c9
yuusan33 <214938543+yuusan33@users.noreply.github.com>
2026-01-14 14:05:01 +0900

    修正：マイグレーションファイルを修正

--- db/migrate/20251003044434_create_tasks.rb
+++ db/migrate/20251003044434_create_tasks.rb
@@ -1,8 +1,8 @@
 class CreateTasks < ActiveRecord::Migration[8.0]
   def change
     create_table :tasks do |t|
-      t.string :title
-      t.boolean :done
+      t.string :title, null: false
+      t.boolean :done, default: false
 
       t.timestamps
     end
